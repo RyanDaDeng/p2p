22cdc6cb63e5ff6b2472a40ee228976f93a99430
ryan <woden6969696969@hotmail.com>
2025-10-28 13:12:39 +1100

    11

--- config/twilio.php
+++ config/twilio.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'account_sid' => env('TWILIO_ACCOUNT_SID', 'ACd6dfd83bd9e07248207699053c344668'),
+    'account_sid' => env('TWILIO_ACCOUNT_SID', ''),
     'auth_token' => env('TWILIO_AUTH_TOKEN'),
-    'verify_service_sid' => env('TWILIO_VERIFY_SERVICE_SID', 'VA7298f3d1601934895945657d8ec9a4bf'),
+    'verify_service_sid' => env('TWILIO_VERIFY_SERVICE_SID', ''),
 ];
